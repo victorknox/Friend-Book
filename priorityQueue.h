ebc47fdc04e03ed9149ed97e7377f0f9eb2035f9
Nitin Shrinivas <nitinshrinivas7@gmail.com>
2021-05-03 11:03:24 +0530

    Functions in pripority queue with checking

--- priorityQueue.h
+++ priorityQueue.h
@@ -1,15 +1,11 @@
-#ifndef __HEAP_H
-#define __HEAP_H
+#ifndef __HEE_H
+#define __HEE_H
 
 #define max_size 100000
 
-typedef int ElementType;
-
-int pQueue[max_size];
-int front_,rear_;
-
-void insert(int element);
-int extractMin();
-
+long extractMin();                           //return the min element and then makes the heap a min heap again.
+void InsertToHeap(long element);             // inserts the element into the heap and then heapify.
+int CheckIsEmpty();                          //returns 0 if the heap is empty.
+                    
 
 #endif
