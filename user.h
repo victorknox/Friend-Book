7e0989ae20874f1d11125f7ab2ace55f6e8999b1
Sambasai <72680171+Sambasai@users.noreply.github.com>
2021-05-06 17:03:17 +0530

    Update user.h

--- user.h
+++ user.h
@@ -1,26 +1,23 @@
+#include "vector.h"
+#include "aflist.h"
+
 #ifndef __USER_H
 #define __USER_H
 
-typedef struct adjacency_list
-{
-    int ID[1000];   //Placeholder
-                    //This has to be done using vectors and hashtables 
-}list;
-
 typedef struct user_profile
 {
-    unsigned int ID;
-    char* name;
-    char gender; //m for male, f for female, o for other, etc.
-    unsigned short int age;
-    char* city;
-    char* country;
-    char* hobby; //Contains only 1 hobby (User's favourite hobby)
-    unsigned short int cfrating;
-    char* organization;
+      unsigned int ID;
+      char *name;
+      char gender; //m for male, f for female, o for other, etc.
+      unsigned short int age;
+      char *city;
+      char *country;
+      char *hobby; //Contains only 1 hobby (User's favourite hobby)
+      unsigned short int cfrating;
+      char *organization;
 
-    list followers;
-    list following;
-}user;
+      friends *flist;
+      antifriends *aflist;
+} user;
 
 #endif
