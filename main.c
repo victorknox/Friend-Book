df3b3b395779795506c98be9c22d7c6c5c1af8e1
Vedant Parekh <vedantbparekh@gmail.com>
2021-05-09 18:31:54 +0530

    Adds comments

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 extern unsigned int MASTER;
 
-void printUI()
+void printUI() //This function prints the list of commands the user can use
 {
     // system("clear");
     printf("\n\n\t\t  *** Friend Book ***\n");
@@ -28,26 +28,28 @@ void printUI()
     //acceptinput();
 }
 
-int acceptinput(user_list *list, struct minHeap *heap)
+int acceptinput(user_list *list, struct minHeap *heap) 
 {
+    //This function acccepts the command from the user and calls the respective function 
     printf("\nEnter choice: ");
     char inp;
     long long a, b;
     scanf(" %c", &inp);
     while ((getchar()) != '\n')
         ;
-    if (inp == '1')
+    if (inp == '1') //User wants to register new user
     {
         printf("Registering new user\n");
         user *temp = TakeInput(heap);
-        list->array_of_users[temp->ID] = temp;
-        resize_user_array(list);
+        list->array_of_users[temp->ID] = temp; //storing the struct returned by TakeInput in the array of user structs
+        resize_user_array(list); //list is a dynamic array of structs and its capacity is updated by this function
 
         recommend_new(list, temp->ID);
         printf("Taking you to the main menu...\n");
     }
     else if (inp == '2')
     {
+        //To unregister the existing user 
         printf("Unregister existing user\n");
         printf("Enter user ID :");
         scanf("%lld", &a);
@@ -55,11 +57,13 @@ int acceptinput(user_list *list, struct minHeap *heap)
     }
     else if (inp == '3')
     {
+        //An already registered user wants friend recommendations
         printf("Recommend friends to existing user\n");
         printf("Enter user ID: ");
         scanf("%lld", &a);
         if (a <= 0 || a > MASTER || list->array_of_users[a] == NULL)
         {
+            //We check if the user id is valid or not before recommending
             printf("\n%lld is not a registered user !", a);
             printf("\nTaking you back to the main menu...");
         }
@@ -73,13 +77,16 @@ int acceptinput(user_list *list, struct minHeap *heap)
     }
     else if (inp == '4')
     {
+        //Adds friend to the user by taking user ids of two users
         printf("Add friend\n");
         AddSingleFriend(list);
     }
     else if (inp == '5')
     {
+        //Checks friendship status of two users
         printf("Check friendship status! List 2 user IDs!\n");
         scanf("%lld%lld", &a, &b);
+        //We check if either of the user ID's are invalid bfore checking their friendship status
         if (a <= 0 || a > MASTER || list->array_of_users[a] == NULL)
         {
             printf("\n%lld is not a registered user !", a);
@@ -92,8 +99,9 @@ int acceptinput(user_list *list, struct minHeap *heap)
         }
         else
         {
-            if (checkfriendshipstatus(list->array_of_users[a]->following, b) == 1)
+            if (checkfriendshipstatus(list->array_of_users[a]->following, b) == 1)//If both user ids are valid it checks the friendship status
             {
+                //It calls the check friendship status function to check for b in the friends list of a
                 printf("\nYes, they are friends!");
             }
             else
@@ -103,29 +111,32 @@ int acceptinput(user_list *list, struct minHeap *heap)
             printf("\nTaking you back to the main menu...\n");
         }
     }
-    else if (inp == '6')
+    else if (inp == '6') 
     {
+        //Prints details of the user whose ID is mentioned
         printf("Printing parameters of a user\n");
         long long tempID;
         printf("Enter user ID: ");
         scanf("%lld", &tempID);
-        if (tempID <= 0 || tempID > MASTER || list->array_of_users[tempID] == NULL)
+        if (tempID <= 0 || tempID > MASTER || list->array_of_users[tempID] == NULL) //Checks if ID is valid
         {
             printf("\n%lld is not a registered user !", tempID);
             printf("\nTaking you back to the main menu...");
         }
         else
         {
-            userDetails(list, tempID);
+            userDetails(list, tempID); //Function to print user details
         }
     }
     else if (inp == '0')
     {
-        printf("Exiting!\nThank you for using this service\n");
+        //To exit the program
+        printf("Exiting!\nThank you for using this service\n"); 
     }
     else
     {
-        printf("Invalid Input! Please try again!\n");
+        //If the input is invalid (i.e,it is not a number from 0 to 6)
+        printf("Invalid Input! Please try again!\n"); 
         inp = acceptinput(list, heap);
     }
     return inp;
@@ -133,30 +144,30 @@ int acceptinput(user_list *list, struct minHeap *heap)
 
 int main()
 {
-    user_list *list = create_user_list();
-    struct minHeap *heap = initHeap();
+    user_list *list = create_user_list(); //Creates the userlist array by allocating memory
+    struct minHeap *heap = initHeap();    //Creates priority queue
 
     char inp = '1';
     system("clear");
-    while (inp != '0')
+    while (inp != '0') //loop terminates only when input = 0, i.e the command to exit to program
     {
-        printUI();
+        printUI();  //Prints list of commands
         inp = acceptinput(list, heap);
     }
     printf("\n");
 
-    //Free All Memory
-    for (int i = 0; i < list->capacity; i++)
+    //Freeing all the dynamically allocated Memory
+    for (int i = 0; i < list->capacity; i++)        //Freeing the data of the whole list of users
     {
-        if (list->array_of_users[i] != NULL)
+        if (list->array_of_users[i] != NULL)        //Frees the data of individual user
         {
             // printf("Freeing user %d\n", i);
             free(list->array_of_users[i]->city);
             free(list->array_of_users[i]->country);
 
-            removeall(list->array_of_users[i]->followers);
+            removeall(list->array_of_users[i]->followers);      //frees the list of all anti-friends of the person
             free(list->array_of_users[i]->followers);
-            removeall(list->array_of_users[i]->following);
+            removeall(list->array_of_users[i]->following);      //frees the list of all friends of the person
             free(list->array_of_users[i]->following);
 
             free(list->array_of_users[i]->hobby);
@@ -166,8 +177,11 @@ int main()
             free(list->array_of_users[i]);
         }
     }
-    free(list->array_of_users);
-    free(list);
-    freeHeap(heap);
+
+    free(list->array_of_users); //frees array of user structs
+    free(list); //frees user_list struct
+    freeHeap(heap); //frees the priority queue
+
     return 0;
 }
+
